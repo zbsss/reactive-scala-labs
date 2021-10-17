5d313a150abe627a61c9aeb7b18b456d43247a43
Michał Kurleto <mkurleto@qualtrics.com>
2021-10-17 15:45:10 +0200

    finished lab2

--- src/main/scala/EShop/lab2/CartActor.scala
+++ src/main/scala/EShop/lab2/CartActor.scala
@@ -3,6 +3,7 @@ package EShop.lab2
 import akka.actor.{Actor, ActorRef, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -29,14 +30,33 @@ class CartActor extends Actor {
   private val log       = Logging(context.system, this)
   val cartTimerDuration = 5 seconds
 
-  private def scheduleTimer: Cancellable = ???
+  private def scheduleTimer: Cancellable = context.system.scheduler.scheduleOnce(cartTimerDuration, self, ExpireCart)
 
-  def receive: Receive = ???
+  def receive: Receive = empty
 
-  def empty: Receive = ???
+  def empty: Receive = LoggingReceive { case AddItem(item) =>
+    context.become(nonEmpty(Cart.empty.addItem(item), scheduleTimer))
+  }
 
-  def nonEmpty(cart: Cart, timer: Cancellable): Receive = ???
+  def nonEmpty(cart: Cart, timer: Cancellable): Receive = LoggingReceive {
+    case RemoveItem(item) if cart.contains(item) && cart.size == 1 =>
+      timer.cancel()
+      context become empty
+    case RemoveItem(item) if cart.contains(item) =>
+      timer.cancel()
+      context become nonEmpty(cart.removeItem(item), scheduleTimer)
+    case AddItem(item) =>
+      timer.cancel()
+      context become nonEmpty(cart.addItem(item), scheduleTimer)
+    case StartCheckout =>
+      timer.cancel()
+      context become inCheckout(cart)
+    case ExpireCart => context become empty
+  }
 
-  def inCheckout(cart: Cart): Receive = ???
+  def inCheckout(cart: Cart): Receive = LoggingReceive {
+    case ConfirmCheckoutCancelled => context become nonEmpty(cart, scheduleTimer)
+    case ConfirmCheckoutClosed    => context become empty
+  }
 
 }
--- src/main/scala/EShop/lab2/Checkout.scala
+++ src/main/scala/EShop/lab2/Checkout.scala
@@ -4,6 +4,7 @@ import EShop.lab2.Checkout._
 import akka.actor.{Actor, ActorRef, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -37,16 +38,36 @@ class Checkout extends Actor {
   val checkoutTimerDuration = 1 seconds
   val paymentTimerDuration  = 1 seconds
 
-  def receive: Receive = ???
+  private def startCheckoutTimer: Cancellable = scheduler.scheduleOnce(checkoutTimerDuration, self, ExpireCheckout)
+  private def startPaymentTimer: Cancellable  = scheduler.scheduleOnce(paymentTimerDuration, self, ExpirePayment)
 
-  def selectingDelivery(timer: Cancellable): Receive = ???
+  def receive: Receive = LoggingReceive { case StartCheckout =>
+    context become selectingDelivery(startCheckoutTimer)
+  }
 
-  def selectingPaymentMethod(timer: Cancellable): Receive = ???
+  def selectingDelivery(timer: Cancellable): Receive = LoggingReceive {
+    case SelectDeliveryMethod(method) => context become selectingPaymentMethod(timer)
+    case ExpireCheckout               => context become cancelled
+    case CancelCheckout               => timer.cancel(); context become cancelled
+  }
 
-  def processingPayment(timer: Cancellable): Receive = ???
+  def selectingPaymentMethod(timer: Cancellable): Receive = LoggingReceive {
+    case SelectPayment(payment) => timer.cancel(); context become processingPayment(startPaymentTimer)
+    case ExpireCheckout         => context become cancelled
+    case CancelCheckout         => timer.cancel(); context become cancelled
+  }
 
-  def cancelled: Receive = ???
+  def processingPayment(timer: Cancellable): Receive = LoggingReceive {
+    case ConfirmPaymentReceived => timer.cancel(); context become closed
+    case ExpirePayment          => context become cancelled
+    case CancelCheckout         => timer.cancel(); context become cancelled
+  }
 
-  def closed: Receive = ???
+  def cancelled: Receive = LoggingReceive { case _ =>
+    log.info("Checkout Cancelled")
+  }
 
+  def closed: Receive = LoggingReceive { case _ =>
+    log.info("Checkout Closed")
+  }
 }
